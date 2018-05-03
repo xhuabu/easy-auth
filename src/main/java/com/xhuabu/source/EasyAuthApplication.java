632607e3e060b52e3753657fe205216c10493223
zzhoo8 <zzhoo8@qq.com>
2018-05-04 01:28:47 +0800

    调整一些配置

--- src/main/java/com/xhuabu/source/EasyAuthApplication.java
+++ src/main/java/com/xhuabu/source/EasyAuthApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @MapperScan(value = {"com.xhuabu.source.dao"})
 @EnableConfigurationProperties
 @SpringBootApplication
-public class JlAuthApplication {
+public class EasyAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JlAuthApplication.class, args);
+		SpringApplication.run(EasyAuthApplication.class, args);
 	}
 }
